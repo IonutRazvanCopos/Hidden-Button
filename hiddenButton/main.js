1a80aab9c9089ff57df15b5d92817de017f0ac90
IonutRazvanCopos <130033085+IonutRazvanCopos@users.noreply.github.com>
2024-06-12 22:37:00 +0300

    Update main.js

--- hiddenButton/main.js
+++ hiddenButton/main.js
@@ -1,17 +1,16 @@
 let winnerIndex = Math.floor(Math.random() * 3);
 
 let buttons = document.querySelectorAll("button");
-const lose = "red", win = "green";
 const message = document.getElementById("message");
 
 buttons.forEach(function(button, index) {
     button.addEventListener("click", function() {
         if (index === winnerIndex) {
           message.textContent = "Winner";
-          message.style.color = win;
+          message.style.color = "green";
         } else {
           message.textContent = "Loser";
-          message.style.color = lose;
+          message.style.color = "red";
         }
     });
 });
